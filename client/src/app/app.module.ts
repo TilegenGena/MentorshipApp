8b3017692aed3d89a065866a880016a53cc4f3c3
TileGena <tn.asankulov@gmail.com>
2024-01-10 22:32:36 +0600

    Summary: - Create routes     * dashboard     * login     * user-profile - Add notification icon - Reorder `app.component.html` - Move `mentorship.app.module.ts` into `mentorship` folder
    Test:
    Run Server, Client
    Main page with links (dashboard, user-profile, login) leads to new pages

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MentorshipAppModule } from './mentorship-app/mentorship-app.module';
+import { MentorshipAppModule } from './mentorship-app-root/mentorship-app/mentorship-app.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    UserProfileComponent,
   ],
   imports: [
     BrowserModule,
--- client/src/app/mentorship-app-root/mentorship-app/mentorship-app.module.ts
+++ client/src/app/mentorship-app-root/mentorship-app/mentorship-app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MentorshipAppRootComponent } from '../mentorship-app-root/mentorship-app-root.component';
-import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
+import { MentorshipAppRootComponent } from '../mentorship-app-root.component';
+import {  MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [MentorshipAppRootComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
+    MatIconModule,
   ], 
   exports: [
     MentorshipAppRootComponent
