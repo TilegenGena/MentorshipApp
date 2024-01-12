a9f1309aa6f48fa3262d6d590fe991d7b0874b9b
Aisulu Abdraimova <aisulu@inkubasia.com>
2024-01-12 17:21:21 +0600

    Summary: Connect Sequelize with Neon  * Installed sequilze-typescript, sequelize-cli, dotenv, pg-connection-string, pg-hstore  * Created .env file, db details (link from neon) is stored there  * Parsed url from .env file to read db details like: port, username etc
    Test:
    * Run `npm run start:dev`, server compiled without errors

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,11 +3,33 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import 'dotenv/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { parse } from 'pg-connection-string';
 
+const url = process.env.DATABASE_URL!;
+const connectionOptions = parse(url);
+const hostOptions = connectionOptions.host!;
+const db = connectionOptions.database!;
 
-console.log(__dirname)
 @Module({
   imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: hostOptions,
+      port: parseInt(connectionOptions.port || '5432'),
+      username: connectionOptions.user,
+      password: connectionOptions.password,
+      database: db,
+      autoLoadModels: true,
+      synchronize: true,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../../', 'client/dist/client'),
     }),
