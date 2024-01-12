14654bb1d33a78a5e315e7b8283feba4cf7a0d0c
TileGena <tn.asankulov@gmail.com>
2024-01-12 16:11:19 +0600

    Summary: - Update structure of project - Delete components which will not use: `user-profile`, `login` - Create `advice wall` component - Rename `mentorship-app` to `navbar-app` - Update imports
    Test:
    run client, server -> pages should load successfully

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AdviceWallComponent } from './advice-wall/advice-wall.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'user-profile', component: UserProfileComponent },
+  { path: 'advice-wall', component: AdviceWallComponent },
 ];
 
 @NgModule({
