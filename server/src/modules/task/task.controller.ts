c7a966328d1854be01e2b57643b1649a37046385
TileGena <tn.asankulov@gmail.com>
2024-01-15 20:05:28 +0600

    Summary: Create endpoints for CRUD function task - create task.controller - create task.service - create task.interface
    Test:
    - build server, should successfuly build
    - install thunder, possible to make request for CRUD task

--- server/src/modules/task/task.controller.ts
+++ server/src/modules/task/task.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Delete,
+  Param,
+  Put,
+} from '@nestjs/common';
+import { TaskService } from './task.service';
+import { Task } from './task.model';
+import { TaskDTO as TaskInterface } from './task.interface';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+  constructor(private taskService: TaskService) {}
+
+  @Get('all-task')
+  async findAll(): Promise<TaskInterface[]> {
+    return this.taskService.getAllTask();
+  }
+
+  @Get(':id')
+  async getTaskById(@Param('id') id: number): Promise<Task> {
+    return this.taskService.getTaskById(id);
+  }
+
+  @Post('create')
+  async createTask(@Body() taskData: Task): Promise<Task> {
+    return this.taskService.createTask(taskData);
+  }
+
+  @Put(':id')
+  async updateTask(
+    @Param('id') id: number,
+    @Body() taskData: Task,
+  ): Promise<Task> {
+    return this.taskService.updateTask(id, taskData);
+  }
+
+  @Delete(':id')
+  async deleteTask(@Param('id') id: number): Promise<void> {
+    return this.taskService.deleteTask(id);
+  }
+}
