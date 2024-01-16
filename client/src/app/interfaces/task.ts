0a5ed6f71086368c9804426f1ee0b24a430ab1ae
Aisulu Abdraimova <aisulu@inkubasia.com>
2024-01-16 22:12:26 +0600

    Summary: Create/Edit/Delete tasks from UI
    * Installe `sweetalert2` to confirm the deletion of the user
    * Apply ng-class to define the styles for the card  based on status
    * Install `angular2-promise-buttons` will be used in the future
    * Use `datepicker` to choose `dueDate`
    * Use `mat-select` to list out the statuses
    
    Test: I am able to do CRUD function on task
    * Task cards color change based on the

--- client/src/app/interfaces/task.ts
+++ client/src/app/interfaces/task.ts
@@ -1,8 +1,10 @@
-export interface ITask {
+export interface TaskDTO {
   id: number;
   title: string;
   menteeId: number;
   description: string;
   status: string;
   dueDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
 }
