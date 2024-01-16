cbe7ec94d83e6be01e1207e40e81ae604cfcf1f1
TileGena <tn.asankulov@gmail.com>
2024-01-16 22:54:53 +0600

    Summary: Update Navbar * install bootstrap script to index.html * generate mentors component, add routes * update navbar using bootstrap template     - add links to pages     - add action buttons     - styles * edit navbar-app.scss

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { AdviceWallComponent } from './components/advice-wall/advice-wall/advice-wall.component';
+import { MentorsComponent } from './components/mentors/mentors/mentors.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'advice-wall', component: AdviceWallComponent },
+  { path: 'mentors', component: MentorsComponent },
 ];
 
 @NgModule({
