81abce34df62185a2b79f63f57e9350193eadaff
Aisulu Abdraimova <aisulu@inkubasia.com>
2024-01-24 22:57:29 +0600

    Create mentorship request modal and post endpoint
    Summary:
    * Using ngb-bootstrap create a modal
      * Inputs are: `requestMessage`, `startDate`, `endDate`
    * Create a DTO for mentorshipRequest
    * Create a service that sends Post request to the server
    
    Test:
    * Went to Mentors page and saw list of mentors
    * On each mentor there is a button that has Request button
    * When I click on that button, modal pops up with the three inputs

--- client/src/app/components/mentors/mentors/mentors.component.ts
+++ client/src/app/components/mentors/mentors/mentors.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { IMentor } from 'src/app/interfaces/mentor';
 import { MentorDetailsModalComponent } from 'src/app/mentor-details-modal/mentor-details-modal.component';
+import { MentorshipRequestComponent } from 'src/app/mentorship-request/mentorship-request-response';
 import { MentorService } from 'src/app/services/mentor.service';
 
 @Component({
@@ -32,5 +33,13 @@ export class MentorsComponent implements OnInit {
       keyboard: false,
     });
     modalRef.componentInstance.mentor = mentor;
+  }
+
+  requestMentorship(data: any) {
+    const modalRef = this.ngbModal.open(MentorshipRequestComponent, {
+      backdrop: 'static',
+      keyboard: false,
+    });
+    modalRef.componentInstance.data = data;
   }
 }
