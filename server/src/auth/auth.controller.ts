2bcffe8a17494a2a2fdb9b002d80a4f8269760fb
TileGena <tn.asankulov@gmail.com>
2024-01-28 22:50:50 +0600

    Set up Passport module with local strategy
    Summary:
    * install passport
    * set up `auth-modal`, `local-guard`, `local.strategy`
    
    Test:
    Should be able to login correctly

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,14 +1,25 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { User } from 'src/modules/user/user.model';
 import { AuthService } from './auth.service';
+import { Request as RequestType } from 'express';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  // @UseGuards(LocalAuthGuard)
+  // @Post('log_in')
+  // async logIn(@Request() req: RequestType): User {
+  //   return req.user!;
+
+  // return this.authService.getValidatedUser(body.email, body.password);
+  // }
+
+  @UseGuards(LocalAuthGuard)
   @Post('log_in')
-  async logIn(@Body() body: Record<string, any>): Promise<User | null> {
-    return this.authService.getValidatedUser(body.email, body.password);
+  logIn(@Request() req: RequestType): User {
+    return req.user!;
   }
 
   // @Post('/log_out')
