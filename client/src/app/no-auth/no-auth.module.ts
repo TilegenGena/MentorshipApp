7ea95247c82404b77a468b6accbcc14d0d95e450
TileGena <tn.asankulov@gmail.com>
2024-02-02 14:32:28 +0600

    Styling and add links for login & registration pages
    Summary:
    * add links <p> <a> tags for login & registration pages
    * add scss to correctly looking
    * Add RegistrationComponent for `no-auth.module`

--- client/src/app/no-auth/no-auth.module.ts
+++ client/src/app/no-auth/no-auth.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { LoginRootComponent } from './login-root/login-root.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { RegistrationComponent } from '../components/registration/registration.component';
 
 @NgModule({
-  declarations: [LoginRootComponent],
+  declarations: [LoginRootComponent, RegistrationComponent],
   imports: [CommonModule, FormsModule, ReactiveFormsModule, NgxSpinnerModule],
 })
 export class NoAuthModule {}
